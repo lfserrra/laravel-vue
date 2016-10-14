c7606b586b271bb326546d78db579b88afe40197
Luis Fernando Serra <lfserrra@gmail.com>
2016-10-14 16:54:53 -0300

    Entrega - Projeto Navegacao e SPA

--- vue.js/dashboard/dashboard.component.js
+++ vue.js/dashboard/dashboard.component.js
@@ -2,22 +2,29 @@ window.dashboardComponent = Vue.extend({
     template: `
     <style>
         .green{color: green}
-        .grey{color: grey}
         .red{color: red}
     </style>
 
     <h1>{{ title }}</h1>
     <h3>{{ description }}</h3>
+    <h4>
+        <label>
+        <input type="checkbox" v-model="accounts_payable"> Somar valores ainda não pagos
+        </label>
+    </h4>
+
+    <br>
 
     <p><b>Total a Receber: </b> {{ receber | currency 'R$ '}}</p>
-    <p><b>Total a pagar: </b> {{ pagar | currency 'R$' }}</p>
-    <h2>Saldo: {{ receber - pagar | currency 'R$' }}</h2>
+    <p><b>Total a pagar: </b> {{ pagar | currency 'R$ ' }}</p>
+    <h2 :class="{'green': (receber - pagar) >= 0, 'red': (receber - pagar) < 0}">Saldo: {{ receber - pagar | currency 'R$ ' }}</h2>
     `,
 
     data: function () {
         return {
             title: 'Dashboard',
-            description: 'Dashboard sistema financeiro'
+            description: 'Dashboard sistema financeiro',
+            accounts_payable: false
         };
     },
 
@@ -36,11 +43,13 @@ window.dashboardComponent = Vue.extend({
     },
 
     methods: {
-        sum: function(collection, column){
+        sum: function(bills){
             var total = 0;
 
-            for (var i = 0; i < collection.length; i++) {
-                total += parseFloat(collection[i][column]);
+            for (var i = 0; i < bills.length; i++) {
+                if(this.accounts_payable || bills[i].done == 1) {
+                    total += parseFloat(bills[i].value);
+                }
             }
 
             return total;
